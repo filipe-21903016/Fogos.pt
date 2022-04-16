9d4d4f084da852dbdb06633cdbbad03b1eae780c
fzarco <filipezarcocoutinho@gmail.com>
2022-04-16 17:41:44 +0100

    binding in Main activity

--- app/src/main/java/com/filipe/tomas/fogos/MainActivity.kt
+++ app/src/main/java/com/filipe/tomas/fogos/MainActivity.kt
@@ -2,10 +2,72 @@ package com.filipe.tomas.fogos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.filipe.tomas.fogos.databinding.ActivityMainBinding
+import com.filipe.tomas.fogos.models.NavigationManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*
+        if (!screenRotated(savedInstanceState)) {
+            //todo change to navigation manager gotoDashboard
+            //NavigationManager.goToCalculatorFragment(supportFragmentManager)
+        }
+         */
+
     }
+
+    /*
+    override fun onStart() {
+        super.onStart()
+        setSupportActionBar(binding.toolbar)
+        setupDrawerMenu()
+    }
+
+    private fun onClickNavigationItem(item: MenuItem): Boolean {
+        when (item.itemId) {
+            //todo setup routes
+            //R.id.nav_calculator -> NavigationManager.goToCalculatorFragment(supportFragmentManager)
+            //R.id.nav_history -> NavigationManager.goToHistoryFragment(supportFragmentManager)
+        }
+        binding.drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun setupDrawerMenu() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawer, binding.toolbar,
+            R.string.drawer_open, R.string.drawer_close
+        )
+        binding.navDrawer.setNavigationItemSelectedListener {
+            onClickNavigationItem(it)
+        }
+        binding.drawer.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun screenRotated(savedInstanceState: Bundle?): Boolean {
+        return savedInstanceState != null
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawer.isDrawerOpen(GravityCompat.START))
+        {
+            binding.drawer.closeDrawer(GravityCompat.START)
+        } else if (supportFragmentManager.backStackEntryCount == 1)
+        {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
+    }
+     */
 }
---
+++ app/src/main/java/com/filipe/tomas/fogos/models/NavigationManager.kt
@@ -0,0 +1,14 @@
+package com.filipe.tomas.fogos.models
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+object NavigationManager {
+    private fun placeFragment(fm: FragmentManager, fragment: Fragment){
+        val transition = fm.beginTransaction()
+        transition.replace(R.id.frame, fragment)
+        transition.addToBackStack(null)
+        transition.commit(
+    }
+
+}
