0bbb929a6b70447ae71cb57cb8cb6ba4706d5b26
fzarco <filipezarcocoutinho@gmail.com>
2022-04-17 15:32:41 +0100

    Fixed Risk

--- app/src/main/java/com/filipe/tomas/fogos/Risk.kt
+++ app/src/main/java/com/filipe/tomas/fogos/Risk.kt
@@ -1,19 +1,8 @@
 package com.filipe.tomas.fogos
-
-import com.filipe.tomas.fogos.R
-
 object Risk{
-    /*
-    Lower(R.string.lower),
-    Moderated(R.string.moderated),
-    High(R.string.high),
-    VeryHigh(R.string.veryHigh),
-    Max(R.string.max)
-     */
-
     fun getLowerRisk() = R.string.lower
-    fun getModeratedRisk() = R.string.lower
-    fun getHighRisk() = R.string.lower
-    fun getVeryHighRisk() = R.string.lower
-    fun getMaxRisk() = R.string.lower
+    fun getModeratedRisk() = R.string.moderated
+    fun getHighRisk() = R.string.high
+    fun getVeryHighRisk() = R.string.veryHigh
+    fun getMaxRisk() = R.string.max
 }
