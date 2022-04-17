d7b5dcb0fd61188b1bb867d083c7ad6476242e57
fzarco <filipezarcocoutinho@gmail.com>
2022-04-17 15:29:59 +0100

    test risk

---
+++ app/src/main/java/com/filipe/tomas/fogos/Risk.kt
@@ -0,0 +1,19 @@
+package com.filipe.tomas.fogos
+
+import com.filipe.tomas.fogos.R
+
+object Risk{
+    /*
+    Lower(R.string.lower),
+    Moderated(R.string.moderated),
+    High(R.string.high),
+    VeryHigh(R.string.veryHigh),
+    Max(R.string.max)
+     */
+
+    fun getLowerRisk() = R.string.lower
+    fun getModeratedRisk() = R.string.lower
+    fun getHighRisk() = R.string.lower
+    fun getVeryHighRisk() = R.string.lower
+    fun getMaxRisk() = R.string.lower
+}
--- app/src/main/java/com/filipe/tomas/fogos/RiskZoneFragment.kt
+++ app/src/main/java/com/filipe/tomas/fogos/RiskZoneFragment.kt
@@ -6,25 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.filipe.tomas.fogos.R
 import com.filipe.tomas.fogos.databinding.FragmentRiskZoneBinding
-import com.filipe.tomas.fogos.models.Risk
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 private const val ARG_RISKZONE = "param1"
 
 class RiskZoneFragment : Fragment() {
     private lateinit var binding: FragmentRiskZoneBinding
-    private var riskZone: String = Risk.Lower.value.toString()
+    //private var riskZone: String = Risk.Lower.value.toString()
 
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             riskZone = it.getString(ARG_RISKZONE).toString()
         }
     }
+     */
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,7 +37,7 @@ class RiskZoneFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onStart() {
         super.onStart()
-        binding.textViewRisk.text = "${getString(R.string.risk)}: ${Risk.VeryHigh}"
+        binding.textViewRisk.text = "${getString(R.string.risk)}: ${Risk.getHighRisk()}"
     }
 
     companion object {
