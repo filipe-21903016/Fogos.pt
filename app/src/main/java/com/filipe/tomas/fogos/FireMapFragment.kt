31f22b7b08e7b6b08bf64794741b4bb33d337fcc
fzarco <filipezarcocoutinho@gmail.com>
2022-04-17 14:53:36 +0100

    changed fire map title

--- app/src/main/java/com/filipe/tomas/fogos/FireMapFragment.kt
+++ app/src/main/java/com/filipe/tomas/fogos/FireMapFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.filipe.tomas.fogos.R
+import com.filipe.tomas.fogos.databinding.FragmentFireMapBinding
 
 
 class FireMapFragment : Fragment() {
+    private lateinit var binding: FragmentFireMapBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = getString(R.string.fire_map)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fire_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_fire_map, container, false)
+        binding = FragmentFireMapBinding.bind(view)
+        return binding.root
     }
 }
