4bb9b28789551afd558dc93a7d33235adb5af4f5
TomasNeto-a21903361 <a21903361@alunos.ulht.pt>
2022-04-17 17:01:12 +0100

    risks

--- app/src/main/java/com/filipe/tomas/fogos/RiskZoneFragment.kt
+++ app/src/main/java/com/filipe/tomas/fogos/RiskZoneFragment.kt
@@ -2,13 +2,20 @@ package com.filipe.tomas.fogos
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.filipe.tomas.fogos.databinding.FragmentRiskZoneBinding
+import java.util.*
+import kotlin.concurrent.schedule
+
 
 private const val ARG_RISKZONE = "param1"
+
+
 
 class RiskZoneFragment : Fragment() {
     private lateinit var binding: FragmentRiskZoneBinding
@@ -37,8 +44,28 @@ class RiskZoneFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onStart() {
         super.onStart()
-        binding.textViewRisk.text = "${getString(R.string.risk)}: ${getString(Risk.getHighRisk())}"
+        loopRisk()
+        loopRisk()
     }
+
+    @SuppressLint("SetTextI18n")
+     fun loopRisk() {
+        binding.textViewRisk.text = "${getString(R.string.risk)}: ${getString(Risk.getLowerRisk())}"
+        Timer().scheduleAtFixedRate(,0,2000){
+            binding.textViewRisk.text = "${getString(R.string.risk)}: ${getString(Risk.getModeratedRisk())}"
+        }
+        Timer().scheduleAtFixedRate(4000){
+            binding.textViewRisk.text = "${getString(R.string.risk)}: ${getString(Risk.getHighRisk())}"
+        }
+        Timer().scheduleAtFixedRate(6000){
+            binding.textViewRisk.text = "${getString(R.string.risk)}: ${getString(Risk.getVeryHighRisk())}"
+        }
+        Timer().scheduleAtFixedRate(8000){
+            binding.textViewRisk.text = "${getString(R.string.risk)}: ${getString(Risk.getMaxRisk())}"
+        }
+
+    }
+
 
     companion object {
 
