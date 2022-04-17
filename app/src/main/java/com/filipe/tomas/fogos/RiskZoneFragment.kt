273fb520168708e901600f459b0d589600cf39f4
TomasNeto-a21903361 <a21903361@alunos.ulht.pt>
2022-04-17 14:58:45 +0100

    risk fire

--- app/src/main/java/com/filipe/tomas/fogos/RiskZoneFragment.kt
+++ app/src/main/java/com/filipe/tomas/fogos/RiskZoneFragment.kt
@@ -9,12 +9,9 @@ import com.filipe.tomas.fogos.R
 import com.filipe.tomas.fogos.databinding.FragmentRiskZoneBinding
 import com.filipe.tomas.fogos.models.Risk
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_RISKZONE = "param1"
 
 class RiskZoneFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private lateinit var binding: FragmentRiskZoneBinding
     private var riskZone: String = Risk.Lower.value.toString()
 
@@ -35,15 +32,7 @@ class RiskZoneFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RiskZoneFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(risk: String) =
             RiskZoneFragment().apply {
